48c50e00fe58f47fa7104316412000e72b434158
YoungDal, Kwon <cool8519@gmail.com>
2023-12-20 20:29:52 +0900

    Fixed a bug in calculation expression

--- src/dal/util/MathUtil.java
+++ src/dal/util/MathUtil.java
@@ -1,5 +1,6 @@
 package dal.util;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import org.apache.log4j.Level;
@@ -10,13 +11,17 @@ import fr.expression4j.factory.ExpressionFactory;
 
 public class MathUtil {
 
+	public static DecimalFormat decimalFormat = new DecimalFormat("#.##############");
+	
 	static {
 		Logger.getLogger("fr.expression4j").setLevel(Level.OFF);
 	}
 	
     public static double evaluateExpression(String expression, List<Double> variables) throws Exception {
     	for (int i = 0; i < variables.size(); i++) {
-            expression = expression.replace("$" + (i + 1), String.valueOf(variables.get(i)));
+    		double value = variables.get(i);
+    		if(Double.isNaN(value)) continue;
+            expression = expression.replaceAll("\\$" + (i+1) + "(?=[^\\d]|$)", decimalFormat.format(value));
         }
     	expression = expression.replace(" ", "");
     	Expression expr = ExpressionFactory.createExpression("f()=" + expression);
